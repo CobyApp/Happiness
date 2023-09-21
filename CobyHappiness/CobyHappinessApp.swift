711f88b281cb356f08d7fa3665c1e09a448f1bf7
coby5502 <coby5502@gmail.com>
2023-09-21 20:40:45 +0900

    Initial Commit

---
+++ CobyHappiness/CobyHappinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobyHappinessApp.swift
+//  CobyHappiness
+//
+//  Created by COBY_PRO on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CobyHappinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
