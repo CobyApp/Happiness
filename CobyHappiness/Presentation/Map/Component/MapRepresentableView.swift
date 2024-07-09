65fa09396ada7f13f6587beb62d78019281497a6
coby <coby5502@gmail.com>
2024-07-09 09:41:20 +0900

    위치권한 삭제

--- CobyHappiness/Presentation/Map/Component/MapRepresentableView.swift
+++ CobyHappiness/Presentation/Map/Component/MapRepresentableView.swift
@@ -104,13 +104,10 @@ struct MapRepresentableView: UIViewRepresentable {
     class Coordinator: NSObject, MKMapViewDelegate, CLLocationManagerDelegate {
         var parent: MapRepresentableView
         var mapView: MKMapView?
-        var locationManager = CLLocationManager()
         
         init(parent: MapRepresentableView) {
             self.parent = parent
             super.init()
-            self.locationManager.delegate = self
-            self.locationManager.requestWhenInUseAuthorization()
         }
         
         func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
@@ -130,21 +127,6 @@ struct MapRepresentableView: UIViewRepresentable {
                 annotationView.annotation = annotation
             }
             return annotationView
-        }
-        
-        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-            if status == .authorizedWhenInUse || status == .authorizedAlways {
-                self.locationManager.startUpdatingLocation()
-            }
-        }
-        
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            if let location = locations.first {
-                let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-                let region = MKCoordinateRegion(center: location.coordinate, span: span)
-                mapView?.setRegion(region, animated: true)
-                self.locationManager.stopUpdatingLocation() // 위치 업데이트 중지
-            }
         }
     }
     
