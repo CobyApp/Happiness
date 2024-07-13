da47fd80a9d1bd7932ac4cae407d0f69f3c45892
Coby <coby5502@gmail.com>
2024-07-14 02:59:00 +0900

    오류 수정

--- CobyHappiness/Presentation/Setting/SettingStore.swift
+++ CobyHappiness/Presentation/Setting/SettingStore.swift
@@ -56,7 +56,7 @@ struct SettingStore: Reducer {
                     return .none
                 }
             case .confirmAlert:
-                return .send(.none)
+                return .none
             case .showThemeView:
                 state.theme = ThemeStore.State()
                 return .none
